1a0e81d19c502e9fdcce8ee8c309c2295841e900
Jorge Arellano <jorge.arellano@mitchell.com>
2021-08-10 11:06:30 -0700

    module 04 complete

---
+++ Tasker/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tasker
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -9,13 +9,24 @@ namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
         [BindProperty]
         public Task NewTask { get; set; }
 
+        public CreateTaskModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
         }
 
-        public IActionResult OnPost() => RedirectToPage("Index");
+        public IActionResult OnPost()
+        {
+            _context.Tasks.Add(NewTask);
+            _context.SaveChanges();
+            return RedirectToPage("Index");
+        }
     }
 }
